ff9db98de3da8034a357811c7df3c04ce4d752c9
julianarios03 <julianarios2611@gmail.com>
2023-10-03 08:54:11 -0500

    se adelanta

---
+++ data/empleados.py
@@ -0,0 +1,55 @@
+Empleado1={
+    "id":1,
+    "Nombre":"Juliana Andrea",
+    "Apellidos":"Rios Guarin",
+    "SalarioMensual":15000000,
+    "Deudas":"True",
+    "RetencionFuente":0.1,
+    "Correo":"JulianaRios2611@gmail.com",
+    "Telefono":"3113706433",
+    "Cargo":"Arquitecto"
+    
+}
+Empleado2={
+    "id":2,
+    "Nombre":"Daniela ",
+    "Apellidos":"Lopez",
+    "SalarioMensual":12000000,
+    "Deudas":"True",
+    "RetencionFuente":0.1,
+    "Correo":"daniela11@gmail.com",
+    "Telefono":"3123745433",
+    "Cargo":"Arquitecto"
+    
+}
+Empleado3={
+    "id":3,
+    "Nombre":"Reachell",
+    "Apellidos":"Rivera",
+    "SalarioMensual":9000000,
+    "Deudas":"True",
+    "RetencionFuente":0.1,
+    "Correo":"Reachell@gmail.com",
+    "Telefono":"3126853649",
+    "Cargo":"Arquitecto"
+    
+}
+Empleado4={
+    "id":4,
+    "Nombre":"Juliana Andrea",
+    "Apellidos":"Rios Guarin",
+    "SalarioMensual":15000000,
+    "Deudas":"True",
+    "RetencionFuente":0.1,
+    "Correo":"JulianaRios2611@gmail.com",
+    "Telefono":"3113706433",
+    "Cargo":"Arquitecto"
+    
+}
+
+#desarollo junior
+#desarollo intermedio
+#desarollo avanzado 
+#arquitecto 
+
+#Solo si se gana mas de 6 millones se aplica retencion en la fuente del 10%
